077712756d3a0535c384975f947c49332bf30615
Jon Scheaffer <84929443+J-Schea29@users.noreply.github.com>
2021-08-31 14:32:28 -0700

    Update app.py

--- Api/app.py
+++ Api/app.py
@@ -1,5 +1,22 @@
 from flask import Flask, jsonify
+from numpy import np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///sqlalchemy-challenge/Resources/hawaii.sqlite")
+inspector = inspect(engine)
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+# Create our session (link) from Python to the DB
+session = Session(engine)
 
 #Flask Setup
 app = Flask(__name__)
@@ -17,13 +34,19 @@ def welcome():
         "/api/v1.0/<start><br>"
         "/api/v1.0/<start>/<end><br>"
     )
-
 @app.route("/api/v1.0/precipitation")
+def precip():
+    precipitation = session.query(Measurement.date, Measurement.prcp).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
 
 @app.route("/api/v1.0/stations")
+def stations():
+    station = session.query(Station.station).all()
+    stations = list(np.ravel(station))
+    return jsonify(stations)
 
-@app.route("/api/v1.0/tobs")
-
-@app.route("/api/v1.0/<start>")
-
-@app.route("/api/v1.0/<start>/<end>")
+# @app.route("/api/v1.0/tobs")
+# def tobs():
+if __name__ == "__main__":
+    app.run(debug=True)    
